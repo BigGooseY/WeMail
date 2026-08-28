83a7401b78d9ec768af5251b0bea8cc3acc0765e
BigGoose <88437946+BigGooseY@users.noreply.github.com>
2026-08-28 13:05:40 +0800

    Simplify WeMail UI to a functional flat design

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 import "../shared/styles/index.css";
+import "../shared/styles/minimal-ui.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
